a2fe0053c5260fffc7fea07ca60b8be5ef71f645
raonytog <raony.gomes@edu.ufes.br>
2024-05-28 14:32:03 -0300

    comecando a tela de selecao

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import personagens
+import os
 
 pygame.init()
 
@@ -14,24 +15,64 @@ SIZE = (WIDTH, HEIGHT)
 SCREEN = pygame.display.set_mode(SIZE)
 
 # definindo o background do jogo e ajustando seu tamanho
-BACKGROUND = pygame.image.load("./imgs/background.png")
+BACKGROUND = pygame.image.load(os.path.join('imgs', "background.png"))
 BACKGROUND = pygame.transform.scale(BACKGROUND, SIZE)
 
 # titulo do jogo
 pygame.display.set_caption("Introbattle")
 
+# retangulo de selecao
+SELECTION_RECTANGLE = pygame.image.load(os.path.join('imgs', "frame.png"))
+
 # personagens
-character_list = list()
-p1 = personagens.Personagem("Maga", 100, 30, 20, 45, 1, True)
-p2 = personagens.Personagem("Maga", 100, 30, 20, 45, 2, True)
-p3 = personagens.Personagem("Maga", 100, 30, 20, 45, 3, True)
-p4 = personagens.Personagem("Maga", 100, 30, 20, 45, 1, False)
-p5 = personagens.Personagem("Maga", 100, 30, 20, 45, 2, False)
-character_list.append(p1)
-character_list.append(p2)
-character_list.append(p3)
-character_list.append(p4)
-character_list.append(p5)
+CHARACTER_LIST = list()
+CHARACTER_LIST.append(personagens.Personagem("Maga", 100, 30, 20, 45, 1, True))
+CHARACTER_LIST.append(personagens.Personagem("Maga", 100, 30, 20, 45, 2, True))
+CHARACTER_LIST.append(personagens.Personagem("Maga", 100, 30, 20, 45, 3, True))
+CHARACTER_LIST.append(personagens.Personagem("Maga", 100, 30, 20, 45, 1, False))
+CHARACTER_LIST.append(personagens.Personagem("Maga", 100, 30, 20, 45, 2, False))
+
+
+def draw_character_select(SCREEN, CHARACTER_LIST):
+    """_summary_
+        Desenha a tela de selecao dos personagens para escolha dos quais 
+        participarao do decorrer do jogo
+
+    Args:
+        SCREEN (SURFACE): tela redimencionada 
+        CHARACTER_LIST (LIST): lista de personagens
+
+    Returns:
+        LIST: Lista de personagens selecionados
+    """
+    
+    count = 0
+    PRESENT_CHARACTER = list()
+    
+    SCREEN.fill(255, 255, 255)
+    for character in CHARACTER_LIST:
+        character.draw_character(SCREEN)
+        
+    return PRESENT_CHARACTER
+        
+
+def draw_screen(SCREEN, CHARACTER_LIST):
+    """_summary_
+        Dsenha uma tela com todos os personagens presentes no jogo
+
+    Args:
+        SCREEN (SURFACE): tela redimencionada 
+        CHARACTER_LIST (LIST): lista de personagens
+    """
+    
+    SCREEN.fill(color="WHITE")
+    SCREEN.blit(BACKGROUND, START)
+
+    for character in CHARACTER_LIST:
+        character.draw_character(SCREEN)
+
+    clock.tick(FPS)  # Definir a taxa de quadros
+    pygame.display.flip()  # Atualizar a exibição
 
 
 def main():
@@ -41,15 +82,11 @@ def main():
             if event.type == pygame.QUIT:
                 end = True
 
-        # desenha a janela com o fundo
-        SCREEN.blit(BACKGROUND, START)
+        keys_pressed = pygame.key.get_pressed()
 
-        # gera personagens
-        for character in character_list:
-            character.draw_character(SCREEN)
 
-        clock.tick(FPS)  # Definir a taxa de quadros
-        pygame.display.flip()  # Atualizar a exibição
+        # desenha toda a game screen
+        draw_screen(SCREEN, CHARACTER_LIST)
 
     pygame.quit()
 
--- personagens.py
+++ personagens.py
@@ -30,7 +30,7 @@ class Personagem(pygame.sprite.Sprite):
         self.attack = attack
         self.name = name
 
-        self.img = pygame.image.load('imgs', f"{self.name}.png")
+        self.img = pygame.image.load(os.path.join('imgs', f"{self.name}.png"))
         self.rect = self.img.get_rect()
 
         if is_hero is True:
@@ -44,4 +44,22 @@ class Personagem(pygame.sprite.Sprite):
 
     def draw_character(self, screen):
         screen.blit(self.img, self.rect)
+
+    def get_character_life_points(self):
+        return self.life_points
+    
+    def get_character_defense(self):
+        return self.defense
+    
+    def get_character_speed(self):
+        return self.speed
+    
+    def get_character_attack(self):
+        return self.attack
+    
+    def get_character_name(self):
+        return self.name
+    
+    def get_caracter_rect(self):
+        return self.rect
 
